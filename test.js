a1d8783b60ea8b58af255e114d9cd3561da62bcd
Zodsmar <zodsmar@gmail.com>
2019-06-17 23:39:22 -0400

    working....

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 setTimeout(() => {
     let geometry = new window.THREE.BoxBufferGeometry(0.2, 0.2, 0.2);
-
+   
     const testDiv = document.getElementById('test');
     
     console.log(testDiv.innerHTML);
